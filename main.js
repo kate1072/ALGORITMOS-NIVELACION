92d730cfedaf454784ac8c7ab89440606f4a0ac3
kate1072 <102970216+kate1072@users.noreply.github.com>
2022-04-05 11:46:07 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+
+     /*funciones*/
+     function falgoritmos(){
+
+     function fejercicio1(){
+        var selector = document.getElementById("algoritmos");
+        selector.classList.toggle("ejercicio1");
+    selector.className
+    }
+    
+    function fejercicio2(){
+        var selector = document.getElementById("algoritmos");
+        selector.classList.toggle("ejercicio2");
+        selector.className
+    }
+    
+    function ejercicio3(){
+        var selector = document.getElementById("algoritmos");
+        selector.classList.toggle("ejercicio3");
+        selector.className
+    }
+    
+    function ejercicio4(){
+        var selector = document.getElementById("algoritmos");
+        selector.classList.toggle("ejercicio4");
+        selector.className
+    }
+    
+    function ejercicio5(){
+        var selector = document.getElementById("algoritmos");
+        selector.classList.toggle("ejercicio5");
+        selector.className
+    }
+    
+    
+    function ejercicio6(){
+        var selector = document.getElementById("algoritmos");
+        selector.classList.toggle("ejercicio6");
+        selector.className
+    }
+    
+    function ejercicio7(){
+        var selector = document.getElementById("algoritmos");
+        selector.classList.toggle("ejercicio7");
+        selector.className
+    }
+    
+    
+    function ejercicio8(){
+        var selector = document.getElementById("algoritmos");
+        selector.classList.toggle("ejercicio8");
+        selector.className
+    }
+    function ejercicio9(){
+        var selector = document.getElementById("algoritmos");
+        selector.classList.toggle("ejercicio9");
+        selector.className
+    }
+    function ejercicio10(){
+        var selector = document.getElementById("algoritmos");
+        selector.classList.toggle("ejercicio10");
+        selector.className
+    }
+    
+}
